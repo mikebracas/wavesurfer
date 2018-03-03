5362d1b7e58e64226389af95556fcfc55c0e1ebe
Miguel Bracamontes <miguel.bracamontes@globaloxs.com>
2018-03-03 13:42:58 -0600

    Init extension

---
+++ Wavesurfer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace mikebraca\wavesurfer;
+
+/**
+ * This is just an example.
+ */
+class Wavesurfer extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
